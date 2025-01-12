f5e8543c0f0890b57fe1a599c22dffd30205aba9
HamaJjigae <166758235+HamaJjigae@users.noreply.github.com>
2025-01-12 03:06:52 -0700

    i got lost in the sauce at 3am and decided to code the twelve days. its inelegant but has no unecessary checks

--- day2/src/lyrics.rs
+++ day2/src/lyrics.rs
@@ -1,1 +1,39 @@
-pub fn lyrics() {}
+use ordinal::Ordinal;
+
+pub fn lyrics() {
+    let words = [
+        "And a partridge in a pear tree.\n",
+        "Two turtle doves",
+        "Three French hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+    let mut counter = 0;
+
+    //this is a very ugly way to handle the first iteration but it doesnt have uncessary checks
+    println!(
+        "On the {} day of Christmas my true love gave to me: ",
+        Ordinal(counter + 1).to_string()
+    );
+    println!("A partridge in a pear tree.\n");
+    counter += 1;
+
+    while counter != 12 {
+        println!(
+            "On the {} day of Christmas my true love gave to me: ",
+            Ordinal(counter + 1).to_string()
+        );
+        for i in (0..=counter).rev() {
+            println!("{}", words[i]);
+        }
+
+        counter += 1;
+    }
+}
