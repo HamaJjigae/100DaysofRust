69554cfdcb1fe9625cce63417d520b277016a533
HamaJjigae <166758235+HamaJjigae@users.noreply.github.com>
2025-01-13 01:46:35 -0700

    day3 adds two little projects: degree conversion and fibonacci numbers honestly the fibonacci one was a huge learning experience with handling memory caps as well as trying to find ways to make it a bit more efficient (caching)

--- day2/src/degree_calc.rs
+++ day2/src/degree_calc.rs
@@ -1,1 +1,53 @@
-pub fn degree_calc() {}
+use std::io;
+
+pub fn degree_calc() {
+    println!("Please enter 1 for C to F, or 2 for F to C");
+
+    let option = loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => match input.trim().parse::<u32>() {
+                Ok(num) if (1..=2).contains(&num) => {
+                    break num;
+                }
+                _ => {
+                    println!("Please enter a valid option between 1 or 2.");
+                }
+            },
+            Err(_) => {
+                println!("Failed to read line.");
+            }
+        }
+    };
+
+    if option == 1 {
+        println!("Please enter the number of degrees in celsius");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let input: f32 = input.trim().parse().expect("Invalid value");
+
+        let output = input * (9.0 / 5.0) + 32.0;
+
+        println!("{} degrees fahrenheit", output);
+    }
+
+    if option == 2 {
+        println!("Please enter the number of degrees in fahrenheit");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let input: f32 = input.trim().parse().expect("Invalid value");
+
+        let output = (input - 32.0) / 1.8;
+
+        println!("{} degrees celsius", output);
+    }
+}
--- day2/src/fibonacci.rs
+++ day2/src/fibonacci.rs
@@ -1,1 +1,53 @@
-pub fn fibonacci_stuff() {}
+use ordinal::Ordinal;
+use std::collections::HashMap;
+use std::io;
+
+pub fn fibonacci_stuff() {
+    println!("Please enter the number n value for nth element of the fibonacci sequence");
+
+    let mut memo = HashMap::new();
+
+    let nth = loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Invalid read");
+        match input.trim().parse::<u128>() {
+            Ok(num) => {
+                if num > 186 {
+                    println!(
+                        "Please enter a number less than 187...my poor u128 can't take any more..."
+                    );
+                    continue;
+                } else {
+                    break num;
+                }
+            }
+            Err(_) => {
+                println!("Please enter a non-negative number");
+                continue;
+            }
+        };
+    };
+
+    println!(
+        "The {} element of the Fibonacci sequence is: {}",
+        Ordinal(nth),
+        fibo_calc(nth, &mut memo)
+    );
+}
+
+fn fibo_calc(n: u128, memo: &mut HashMap<u128, u128>) -> u128 {
+    if let Some(&result) = memo.get(&n) {
+        return result;
+    }
+
+    let result = if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibo_calc(n - 1, memo) + fibo_calc(n - 2, memo)
+    };
+
+    memo.insert(n, result);
+    result
+}
